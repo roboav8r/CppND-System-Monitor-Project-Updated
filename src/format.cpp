28c271cb4ba63bc78669600a24b0b99c4dc0558f
John Duncan <john.a.duncan@utexas.edu>
2022-08-16 16:40:52 -0500

    Add formatted HH:MM:SS to display

--- src/format.cpp
+++ src/format.cpp
@@ -4,8 +4,16 @@
 
 using std::string;
 
-// TODO: Complete this helper function
 // INPUT: Long int measuring seconds
 // OUTPUT: HH:MM:SS
-// REMOVE: [[maybe_unused]] once you define the function
-string Format::ElapsedTime(long seconds[[maybe_unused]]) { return string(); }
+string Format::ElapsedTime(long seconds) { 
+    int hrs = int( (seconds - seconds%3600) / 3600);
+    int mins = int( (seconds - hrs*3600 - seconds%60) / 60);
+    int secs = int( seconds - hrs*3600 - mins*60 );
+
+    string hr_string = (hrs>=10) ? std::to_string(hrs) : '0' + std::to_string(hrs);
+    string min_string = (mins>=10) ? std::to_string(mins) : '0' + std::to_string(mins);
+    string sec_string = (secs>=10) ? std::to_string(secs) : '0' + std::to_string(secs);
+
+    return hr_string + ":" + min_string + ":" + sec_string; 
+}
